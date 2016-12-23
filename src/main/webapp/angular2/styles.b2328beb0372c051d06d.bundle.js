f0c4f74f339d71e4aa3d667e16bb8347e5bdd553
tommasoborgato <ICTTB0@MIL.ESSELUNGA.NET>
2016-12-23 14:40:26 +0100

    POC Spring REST

--- src/main/webapp/angular2/styles.b2328beb0372c051d06d.bundle.js
+++ src/main/webapp/angular2/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{414:function(n,o){},749:function(n,o,c){n.exports=c(414)}},[749]);
+webpackJsonp([1,3],{416:function(n,o){},754:function(n,o,c){n.exports=c(416)}},[754]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
---
+++ target/POC-1.0-SNAPSHOT/angular2/styles.b2328beb0372c051d06d.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{416:function(n,o){},754:function(n,o,c){n.exports=c(416)}},[754]);
+//# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
